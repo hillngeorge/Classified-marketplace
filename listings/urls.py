e82dd55ebe28469ffc5667af46ca3c156b8c211e
hillngeorge <hillngeorge@gmail.com>
2024-07-27 18:10:28 -0500

    new req

--- listings/urls.py
+++ listings/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('listings/create/', views.listing_create, name='listing_create'),
     path('listings/<slug:slug>/', views.listing_detail, name='listing_detail'),
     path('listings/categories/<int:category_id>/', views.category_listings, name='category_listings'),
-]
+    path('listings/<slug:slug>/edit/', views.listing_edit, name='listing_edit'),
+     ]
