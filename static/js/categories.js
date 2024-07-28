f7671ec54885769416b324fe29bd472973530d7b
hillngeorge <hillngeorge@gmail.com>
2024-07-27 22:31:00 -0500

    test?

--- static/js/categories.js
+++ static/js/categories.js
@@ -10,6 +10,8 @@ function init() {
         const cats = document.querySelectorAll(".category-grid a");
 
         cats.forEach(link => {
+            // current: if the category text contains the search query text
+            // go to: if the category text contains the search query, or the subcategories of the category contains the search query
             if(link.textContent.toLocaleLowerCase().indexOf(search.toLocaleLowerCase()) >= 0) {
                 // show
                 link.classList.remove("hide");
